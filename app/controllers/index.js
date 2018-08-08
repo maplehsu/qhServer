8380816a0d7ecad42dc65446da49e0e6e3bde437
Maple <maplehsuu@gmail.com>
2018-08-09 00:44:37 +0800

    3

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -39,11 +39,20 @@ module.exports = {
     ctx.body = data
   },
   getSelectPath: async (ctx, next) => {
-    console.log(ctx);
-    // let db = config.db('xianlu')
-    // let data = await db.find({},{"content":0}).sort({"creatTime": -1}).limit(3)
-    // ctx.response.type = 'application/json'
-    // ctx.body = data
+    let db = config.db('xianlu')
+    let data = await db.aggregate([
+      {
+      	$project: {"content":0}
+      },
+      {
+        $sort: {"creatTime": -1}
+      },
+      {
+        $limit: parseInt(ctx.request.query.limit)
+      }
+    ])
+    ctx.response.type = 'application/json'
+    ctx.body = data
   },
   getPathList: async (ctx, next) => {
     let db = config.db('xianlu')
